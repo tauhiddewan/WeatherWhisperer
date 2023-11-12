a7270575e991fee34351df9e34412892dcf74536
tauhiddewan <dewan.tauhid.rahman@gmail.com>
2023-11-12 16:54:03 +0600

    updated for ml model api

--- api/server.py
+++ api/server.py
@@ -1,11 +1,14 @@
 import time
+import pickle
+import pandas as pd
 from fastapi import FastAPI
 from api.services.download import get_district_datas, get_hourly_datas
 from api.services.process import get_10_coolest_districts
 
-
-
 names, lats, longs = get_district_datas()
+with open("api/model/xgb_model.pkl", 'rb') as file:
+    model = pickle.load(file)
+
 app = FastAPI()
 @app.get("/")
 def get_coolest_10_districts():
@@ -14,3 +17,10 @@ def get_coolest_10_districts():
     result = get_10_coolest_districts(district_data_responses=responses)
     print(f"Time taken for response : {time.time()-t1}")
     return result
+
+@app.post("/predict")
+def get_temp_forecast(date):
+    new_date = pd.to_datetime(date)
+    day_of_year = new_date.dayofyear
+    predicted_temperature = model.predict([day_of_year])[0]
+    return {"predicted_temp" : predicted_temperature}
